275ca017c2f4ad745489dc89643c38d17adb3759
James Min <jmin499@gmail.com>
2020-10-02 04:05:14 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,26 @@ const codes = [
 
 function init() {
   // your code here
+  let index = 0;
+
+  document.body.addEventListener('keydown', (e) => {
+    // if(codes.includes(e.key)) alert("Hurray")
+    const key = e.key;
+
+    if (codes[index] === key) {
+    index++;
+
+      if (index === codes.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+    } else {
+      index = 0;
+    }
+  });
+
+
 }
+
+init()
