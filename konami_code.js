ce53061053a250d9e0db47c2b5e2f65523e255fe
James Min <jmin499@gmail.com>
2020-10-02 04:04:23 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,21 +17,10 @@ function init() {
 
   document.body.addEventListener('keydown', (e) => {
     // if(codes.includes(e.key)) alert("Hurray")
-
-    const key = e.key;
-
-    if (key === alphabet[index]) {
-      index++;
-
-      if (index === alphabet.length) {
-        alert("Hurray!");
-
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
+    
   });
+
+
 }
 
 init()
